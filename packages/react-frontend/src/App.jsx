c1f363d4b676377c431fbfb7f00318cdff2e4998
Rkdeleon123 <rkdeleon@calpoly.edu>
2026-08-28 14:19:42 -0700

    chanes to app.jsx

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -16,7 +16,10 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+        {/*
+        Change below this
+        */}
+          <h1>Hello World</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
