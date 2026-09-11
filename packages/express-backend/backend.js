e70f95e827b12fcdd0793060c0a00756eca443f0
Rkdeleon123 <rkdeleon@calpoly.edu>
2026-09-11 14:35:58 -0700

    moving to step 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,8 @@
 // backend.js
 import express from "express";
+import cors from "cors";
+
+
 
 const app = express();
 const port = 8000;
@@ -34,6 +37,7 @@ const users = {
   ],
 };
 
+app.use(cors());
 app.use(express.json());
 
 const addUser = (user) => {
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,10 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+
+  function fetchUsers() {
+  const promise = fetch("http://localhost:8000/users");
+  return promise;
+  }
+
+  useEffect(() => {
+  fetchUsers()
+    .then((res) => res.json())
+    .then((json) => setCharacters(json["users_list"]))
+    .catch((error) => {
+      console.log(error);
+    });
+  }, []);
+
+
+  function postUser(person) {
+  const promise = fetch("Http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  });
+
+  return promise;
+  }
+
+  function updateList(person) {
+  postUser(person)
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -13,9 +49,11 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters((currentCharacters) => [...currentCharacters, person]);
-  }
+  // function updateList(person) {
+  //   setCharacters((currentCharacters) => [...currentCharacters, person]);
+  // }
+
+
 
   return (
     <div className="container">
