e8340b1324bd4b1c57d31653dc1a6745a540b7ce
Rkdeleon123 <rkdeleon@calpoly.edu>
2026-09-11 14:21:50 -0700

    fixed

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -62,11 +62,23 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter((user) => user["name"] === name  && user["job"] === job);
+
+};
 
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
+  const job = req.query.job;
+  if (name != undefined && job != undefined){
+    let result = findUserByNameAndJob(name,job);
+    result = { users_list: result };
+    res.send(result);
+
+  }
+
+  else if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
     res.send(result);
@@ -76,6 +88,19 @@ app.get("/users", (req, res) => {
 });
 
 
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
